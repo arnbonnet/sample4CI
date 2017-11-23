9b380133c60e1ab8021d3efcd50e78e7c262fea6
abonnet <abonnet@MTP-D3020-003.DIGINAMIC.LOCAL>
2017-11-23 17:06:13 +0100

    added test

--- src/test/java/entities/UserTest.java
+++ src/test/java/entities/UserTest.java
@@ -2,54 +2,53 @@ package entities;
 
 import org.hamcrest.core.IsEqual;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import entitites.User;
 
 public class UserTest {
+	
+	private User user;
+	private User user2;
+	private User user3;
+	
+	@Before
+	public void before() {
+		user = new User();
+		user.setFirstName("Jean");
+		user.setLastName("Dupont");
+		
+		user2 = new User();
+		user2.setFirstName("Jeanne");
+		user2.setLastName("AuSecours");
+		
+		user3 = new User();
+		user3.setFirstName("Jean");
+		user3.setLastName("Dupont");
+	}
 
 	@Test
 	public void test1() {
-		User user = new User();
-		user.setFirstName("Jean");
 		Assert.assertThat(user.getFirstName(), IsEqual.equalTo("Jean"));
 	}
 
 	@Test
 	public void test2() {
-		User user = new User();
-		user.setLastName("Dupont");
 		Assert.assertThat(user.getLastName(), IsEqual.equalTo("Dupont"));
 	}
 
 	@Test
 	public void testEquals() {
-		User user = new User();
-		user.setFirstName("Jean");
-		user.setLastName("Dupont");
-
-		User user2 = new User();
-		user2.setFirstName("Jeanne");
-		user2.setLastName("AuSecours");
-
-		User user3 = new User();
-		user3.setFirstName("Jean");
-		user3.setLastName("Dupont");
-
 		Assert.assertEquals(false, user.equals(user2));
 		Assert.assertEquals(true, user.equals(user3));
 		Assert.assertEquals(true, user.equals(user));
 		Assert.assertEquals(false, user.equals(null));
 		Assert.assertEquals(false, user.equals(new String("hello")));
-
 	}
 
 	@Test
 	public void testToString() {
-		User user = new User();
-		user.setFirstName("Jean");
-		user.setLastName("Dupont");
-
 		Assert.assertEquals("lastName=Dupont, firstName=Jean", user.toString());
 	}
 
